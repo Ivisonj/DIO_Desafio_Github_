b383014d0395fee6a610267e380ea913401bcc16
Ivisonj <ivison_j@outlook.com>
2022-11-12 20:28:50 -0300

    Acrescimo do desafio 2

--- Exercícios DIO/Exercícios_DIO.js
+++ Exercícios DIO/Exercícios_DIO.js
@@ -1,4 +1,4 @@
-//Dssafio 1---------------------------------------------------------------------------------------------------------------
+//Desafio 1---------------------------------------------------------------------------------------------------------------
 /* 
 Faça um programa para calcular o valor de uma viagem 
 
@@ -8,10 +8,37 @@ você terá 3 variáveis:
 3 - Distância em KM 
 */
 
- const precoCombustivel = 5.79;
- const KmPorLitro = 12;
- const distancia = 1580; 
+//  const precoCombustivel = 5.79;
+//  const KmPorLitro = 12;
+//  const distancia = 1580; 
+
+//  const litrosConsumidos = distancia / KmPorLitro;
+//  const valorGasto = litrosConsumidos * precoCombustivel;
+//  console.log(valorGasto.toFixed(2));
+
+ //-------------------------------------------------------------------------------------------------------------------
+ //---Desafio 2
+/* Faça um programa para calcular o valor de uma viagem 
+
+você terá 5 variáveis: 
+1 - preço do etanol;
+2 - preco da gasolina;
+3 - o tipo de combustível que está no seu carro;
+2 - gasto médio de combustível do carro por KM;
+3 - distância em KM 
+*/
+ const precoDaGasolina = 6.66;
+ const precoDoEtanol = 5.79;
+ const KmPorLitro = 10;
+ const distancia = 100; 
+ const tipoCombustivel = 'Gasolina'
 
  const litrosConsumidos = distancia / KmPorLitro;
- const valorGasto = litrosConsumidos * precoCombustivel;
- console.log(valorGasto.toFixed(2));
+
+ if(tipoCombustivel === 'Etanol'){
+     const valorGasto1 = litrosConsumidos * precoDoEtanol; 
+     console.log(valorGasto1);
+ } else {
+     const valorGasto2 = litrosConsumidos * precoDaGasolina;
+     console.log(valorGasto2);
+ }
